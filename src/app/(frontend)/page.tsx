33b4425e6a6968a748190720ab6621e2f99061ed
vi0letchen <violethongjinchen@gmail.com>
2025-09-13 14:03:41 +1200

    chore:install tailwindcss

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -14,23 +14,31 @@ export default async function HomePage() {
   console.log(homeInfo)
 
   return (
-    <div className="home">
-      <h1>{homeInfo?.Name}</h1>
-      <Image
-        src={(homeInfo?.['Profile Picture'] as Media).url || ''}
-        alt={(homeInfo?.['Profile Picture'] as Media).alt || 'Profile Picture'}
-        width={200}
-        height={200}
-      />
-      <h2>About Me</h2>
-      <div>{homeInfo?.['About Me']}</div>
-      <h2>Contact Details</h2>
-      <div>
-        <b>Email: </b>
-        {homeInfo?.['Email Address']}</div>
-      <div>
-        <b>Phone Number: </b> 
-        {homeInfo?.['Phone Number']}</div>
+    <div className='home'>
+      <div className="page">
+        <h1>{homeInfo?.Name}</h1>
+        <h2>About Me</h2>
+        <div className="flex flex-row">
+          <div>{homeInfo?.['About Me']}</div>
+        </div>
+          <div className="rounded-lg shadow-lg float-right ml-4 mb-2">
+            <Image
+              src={(homeInfo?.['Profile Picture'] as Media).url || ''}
+              alt={(homeInfo?.['Profile Picture'] as Media).alt || 'Profile Picture'}
+              width={200}
+              height={200}
+            />
+          </div>
+        <h2>Contact Details</h2>
+        <div>
+          <b>Email: </b>
+          {homeInfo?.['Email Address']}
+        </div>
+        <div>
+          <b>Phone Number: </b>
+          {homeInfo?.['Phone Number']}
+        </div>
+      </div>
     </div>
   )
 }
